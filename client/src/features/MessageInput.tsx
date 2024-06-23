d9830cbc87fce394e9fc9b496f76103c838438d5
agne.statkeviciute <agne.statkeviciute@oxylabs.io>
2024-06-23 14:36:55 +0300

    fix: send message

--- client/src/features/MessageInput.tsx
+++ client/src/features/MessageInput.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { MessageInputProps } from "./types";
-import { iconChatButton } from "../images";
+import Button from "../components/Button";
 
 const MessageInput: React.FC<MessageInputProps> = ({
   handleUserTyping,
@@ -18,10 +18,62 @@ const MessageInput: React.FC<MessageInputProps> = ({
       value={inputValue}
       onChange={(e) => setInputValue(e.target.value)}
     />
-    <button onClick={handleForm}>
-      <img src={iconChatButton} />
-    </button>
+    <Button
+      text="Send"
+      className="bg-white w-20 mt-5 rounded-2xl text-primary"
+      onClick={handleForm}
+    />
   </div>
 );
 
 export default MessageInput;
+
+// const MessageInput: React.FC<MessageInputProps> = ({
+//   handleUserTyping,
+//   inputValue,
+//   setInputValue,
+//   handleForm,
+// }) => (
+//   <form className="flex md:w-5/12  justify-between md:m-auto">
+//     <input
+//       type="text"
+//       placeholder="Enter Message"
+//       name="message"
+//       onInput={handleUserTyping}
+//       className="w-4/5"
+//       value={inputValue}
+//       onChange={(e) => setInputValue(e.target.value)}
+//     />
+//     <Button
+//       text="Send"
+//       className="bg-white w-20 mt-5 rounded-2xl text-primary"
+//       onClick={handleForm}
+//     />
+//   </form>
+// );
+
+// export default MessageInput;
+
+// const handleForm = (e: any) => {
+//   e.preventDefault();
+//   //Emit message to serve
+//   if (socket)
+//     socket.emit("chatMessage", inputValue, (value: string) => {
+//       console.log(value);
+//     });
+//   if (!!inputValue && inputValue.replace(/\s/g, "") !== "") {
+//     setUserMessage((userMessage) => [
+//       ...userMessage,
+//       {
+//         username: `Me`,
+//         text: `${inputValue}` || "",
+//       },
+//     ]);
+//     setInputValue("");
+//   } else {
+//     return;
+//   }
+// };
+// const handleUserTyping = () => {
+//   socket!.emit("typing", chatRoom, userName);
+// };
